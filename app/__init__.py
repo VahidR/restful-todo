055b2638d1e6aa2aebf7f6d00fdace3478770c62
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2015-01-09 11:03:13 +0100

    Enabling Markdown in TODO list.

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,26 @@
-from flask import Flask, render_templatefrom flask.ext.bootstrap import Bootstrapfrom flask.ext.moment import Momentfrom flask.ext.sqlalchemy import SQLAlchemyfrom config import configbootstrap = Bootstrap()moment = Moment()db = SQLAlchemy()def create_app(config_name):  app = Flask(__name__)  app.config.from_object(config[config_name])  config[config_name].init_app(app)  bootstrap.init_app(app)  moment.init_app(app)  db.init_app(app)  from main import main as main_blueprint  app.register_blueprint(main_blueprint)  return app
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from flaskext.markdown import Markdown
+from config import config
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+
+def create_app(config_name):
+  app = Flask(__name__)
+  app.config.from_object(config[config_name])
+  config[config_name].init_app(app)
+
+  bootstrap.init_app(app)
+  moment.init_app(app)
+  db.init_app(app)
+
+  Markdown(app)
+
+  from app.main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+  return app
